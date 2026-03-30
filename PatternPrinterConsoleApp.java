a0d9e05307097b3582de6d677f2dd5a829616df1
GLORY FIE MENESES <g.meneses.545673@umindanao.edu.ph>
2026-03-30 22:22:20 +0800

    Add files via upload
    Practicing + understanding nested loops/if-else

---
+++ PatternPrinterConsoleApp.java
@@ -0,0 +1,81 @@
+package CCE107Activities;
+
+public class PatternPrinterConsoleApp {
+
+	public static void main(String[] args) {
+		
+		// level 1
+		//task 1: rectangle printer
+		
+	/*	for (int i = 0; i < 5 ; i++) {
+			for (int g = 0; g < 25 ; g++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	*/
+		
+		
+		// task 2: hollow box
+		
+		
+	/*	for (int g = 1; g <= 8; g++) {
+			for (int f = 1; f <= 8; f++) {
+				if (g == 1 || g == 8 || f == 1 || f == 8) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	*/	
+		
+		
+		// level 2
+		//task 3: right triangle
+		
+	/*	for (int i = 1; i <= 10; i++) {
+			for (int g = 1; g <= i; g++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	*/
+		
+		//multiplication table 4x4
+		
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				System.out.print(i*j + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
